b5ea8e8767f9fcd207a7abb2058df55ec78d1ad0
Kushal Paliwal <91718670+finnixx@users.noreply.github.com>
2025-03-02 01:30:13 +0530

    db integerated

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,12 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const prisma = new PrismaClient({
+    datasourceUrl: process.env.DATABASE_URL,
+}).$extends(withAccelerate())
 
 const app = new Hono()
 

e59053e4cb483386705a6fcb383d866ea77bf484
Kushal Paliwal <91718670+finnixx@users.noreply.github.com>
2025-03-13 05:35:19 +0530

    corrections

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -2,17 +2,17 @@ import { FullBlog } from "../components/FullBlog";
 import { useBlog } from "../hooks";
 import { useParams } from "react-router-dom";
 
-export function Blog(){
-    const {id} = useParams();
-    const [loading,blog] = useBlog({
-        id:id||""
+export function Blog() {
+    const { id } = useParams();
+    const { loading, blog } = useBlog({
+        id: id || ""
     });
-    if(loading){
-        return <>
-        Loading... 
-        </>
+
+    if (loading || !blog) {
+        return <>Loading...</>;
     }
+
     return <>
-        <FullBlog/>
-    </>
+        <FullBlog blog={blog} />
+    </>;
 }
